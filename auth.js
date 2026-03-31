2cd07b656ff871a6ccf14b9bd38be819f52bf0f2
Naman Dua <namanworkoff@gmail.com>
2026-03-31 15:32:53 +0530

    Update signup URL to production endpoint

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ function signup() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    fetch("http://127.0.0.1:5000/signup", {
+    fetch("https://aman-render.onrender.com/signup", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
